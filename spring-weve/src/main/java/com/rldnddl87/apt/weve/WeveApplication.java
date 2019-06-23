c4fdfb9f22b62133c4dac5e7bbb639378503bc9b
giungSong <rldnddl87@gmail.com>
2019-06-24 02:15:08 +0900

    토이 프로젝트 생성

---
+++ spring-weve/src/main/java/com/rldnddl87/apt/weve/WeveApplication.java
@@ -0,0 +1,13 @@
+package com.rldnddl87.apt.weve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeveApplication.class, args);
+    }
+
+}
