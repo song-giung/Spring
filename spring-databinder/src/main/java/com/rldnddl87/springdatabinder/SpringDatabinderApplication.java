416af93c9748e43f38f408fdbbfdcef7f352060d
giungSong <rldnddl87@gmail.com>
2019-06-26 22:40:03 +0900

    DataBinding - PropertyEditor 사용 예시

---
+++ spring-databinder/src/main/java/com/rldnddl87/springdatabinder/SpringDatabinderApplication.java
@@ -0,0 +1,13 @@
+package com.rldnddl87.springdatabinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatabinderApplication.class, args);
+    }
+
+}
