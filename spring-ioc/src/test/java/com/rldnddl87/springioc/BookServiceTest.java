a6174efeecc097c221b29c5c4a146da51f3395d2
giungSong <rldnddl87@gmail.com>
2019-06-29 18:48:15 +0900

    @Mock을 통해 ioc와 같은 의존성 주입환경을 만들어봄

--- spring-ioc/src/test/java/com/rldnddl87/springioc/BookServiceTest.java
+++ spring-ioc/src/test/java/com/rldnddl87/springioc/BookServiceTest.java
@@ -1,24 +1,28 @@
 package com.rldnddl87.springioc;
 
+import org.hibernate.validator.constraints.Mod10Check;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookServiceTest {
 
+    @Mock
+    BookRepository bookRepository;
 
-    //BookRepository의 구현 없이 BookService의 save 메서드를 테스트 할 수없다 의존성이 있기 때문에
     @Test
     public void save() {
         Book book = new Book();
 
-        BookRepository bookRepository = new BookRepository();
+        when(bookRepository.save(book)).thenReturn(book); //의존성 주입과 같은 설정
         BookService bookService = new BookService(bookRepository);
 
         Book result = bookService.save(book);
