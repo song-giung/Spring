58600679c8de54974ca3add9e47ada1e7c42a4b3
giungSong <rldnddl87@gmail.com>
2019-06-29 19:59:59 +0900

    같은 타입의 빈이 여러개일 경우 - @Qulifier() // 타입 세이프 X

--- spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/BookService.java
+++ spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/BookService.java
@@ -1,12 +1,13 @@
 package com.rldnddl87.springioc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-    @Autowired
+    @Autowired @Qualifier("rldndBookRepository")
     BookRepository bookRepository;
 
     public void printBookRepository() {
--- spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/RldndBookRepository.java
+++ spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/RldndBookRepository.java
@@ -3,7 +3,7 @@ package com.rldnddl87.springioc;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-@Primary
+
 @Repository
 public class RldndBookRepository implements BookRepository {
 }
