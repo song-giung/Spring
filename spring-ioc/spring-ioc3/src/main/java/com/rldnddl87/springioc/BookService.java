340f677f14fae88b42780ff6ceb8529b8326d321
giungSong <rldnddl87@gmail.com>
2019-06-29 19:49:08 +0900

    @Autowired - 생성자

---
+++ spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/BookService.java
@@ -0,0 +1,15 @@
+package com.rldnddl87.springioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+}
