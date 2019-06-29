08dc21aec05891c47506883d87522bbd5eea17a8
giungSong <rldnddl87@gmail.com>
2019-06-29 20:01:30 +0900

    같은 타입의 빈이 여러개일경우 - 모두 주입 받기

--- spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/BookService.java
+++ spring-ioc/spring-ioc3/src/main/java/com/rldnddl87/springioc/BookService.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
 
-    @Autowired @Qualifier("rldndBookRepository")
-    BookRepository bookRepository;
+    @Autowired
+    List<BookRepository> bookRepository;
 
     public void printBookRepository() {
-        System.out.println(bookRepository.getClass());
+        this.bookRepository.forEach(System.out::println);
     }
 
 }
