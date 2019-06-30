430e27c29a4b999ae96b94ae2fbcd96d5601087c
giungSong <rldnddl87@gmail.com>
2019-06-30 15:44:07 +0900

    MessageSource의 기본적인 사용

---
+++ spring-ioc/spring-ioc6/src/main/java/com/rldnddl87/springioc/AppRunner.java
@@ -0,0 +1,31 @@
+package com.rldnddl87.springioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+       System.out.println(messageSource.getClass());
+       //class org.springframework.context.support.ResourceBundleMessageSource
+
+
+       System.out.println(messageSource.getMessage("greeting", new String[] {"rldnd"}, Locale.KOREA));
+       System.out.println(messageSource.getMessage("greeting", new String[] {"rldnd"}, Locale.getDefault()));
+    }
+}
