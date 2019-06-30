dd7be1184cb2802c6fe44433494ac1f25cbab96e
giungSong <rldnddl87@gmail.com>
2019-06-30 14:53:12 +0900

    Bean의 Scope : Singleton / Proto

---
+++ spring-ioc/spring-ioc4/src/main/java/com/rldnddl87/springioc/Proto.java
@@ -0,0 +1,12 @@
+package com.rldnddl87.springioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("prototype")
+@Component
+public class Proto {
+
+
+}
