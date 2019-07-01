e2c2a24a57418dad849eed667534833b4809c87f
giungSong <rldnddl87@gmail.com>
2019-07-02 00:12:39 +0900

    추가

--- spring-ioc/spring-ioc7/src/main/java/com/rldnddl87/springioc/MyEventHandler.java
+++ spring-ioc/spring-ioc7/src/main/java/com/rldnddl87/springioc/MyEventHandler.java
@@ -1,6 +1,8 @@
 package com.rldnddl87.springioc;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -10,9 +12,23 @@ import org.springframework.stereotype.Component;
 public class MyEventHandler{
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE)
     public void onHandle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받음. 데이터는 " +  event.getData());
     }
+
+
+    //스프링에서 기본적으로 제공하는 이벤트
+    @EventListener
+    public void onHandle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    public void onHandle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
+    }
+
 }
