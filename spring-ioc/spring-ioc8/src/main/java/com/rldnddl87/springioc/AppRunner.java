5e8ec8fec6f013ee84894bd10b324f8be4e13c62
giungSong <rldnddl87@gmail.com>
2019-07-02 00:50:28 +0900

    ResourceLoader와 Resource 추상화

---
+++ spring-ioc/spring-ioc8/src/main/java/com/rldnddl87/springioc/AppRunner.java
@@ -0,0 +1,31 @@
+package com.rldnddl87.springioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+
+    @Autowired
+    ApplicationContext resourceLoader;
+    //ApplicationContext resourceLoader; 가능~
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
+
+
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+
+    }
+}
