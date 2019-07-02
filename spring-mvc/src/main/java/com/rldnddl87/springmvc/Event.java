a257116571f05649476b6ec390478c00d3c86753
giungSong <rldnddl87@gmail.com>
2019-07-03 00:43:48 +0900

    Spring-mvc

---
+++ spring-mvc/src/main/java/com/rldnddl87/springmvc/Event.java
@@ -0,0 +1,21 @@
+package com.rldnddl87.springmvc;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    private String name;
+
+    private int limitOfEnrollment;
+
+    private LocalDateTime startDateTime;
+
+    private LocalDateTime endDateTime;
+}
