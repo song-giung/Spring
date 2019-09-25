98f2c8abcaca7f80b09180d5b78c63f588bc9b88
giungSong <rldnddl87@gmail.com>
2019-09-25 10:30:59 +0900

    spring http message converter - JSON

---
+++ spring-httpmessageconverter/src/main/java/com/rldnddl87/httpmessegeconverter/springhttpmessageconverter/Person.java
@@ -0,0 +1,30 @@
+package com.rldnddl87.httpmessegeconverter.springhttpmessageconverter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- spring-httpmessageconverter/src/test/java/com/rldnddl87/httpmessegeconverter/springhttpmessageconverter/ControllerTest.java
+++ spring-httpmessageconverter/src/test/java/com/rldnddl87/httpmessegeconverter/springhttpmessageconverter/ControllerTest.java
@@ -1,10 +1,12 @@
 package com.rldnddl87.httpmessegeconverter.springhttpmessageconverter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,12 +24,36 @@ public class ControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+
     @Test
-    public void stringMessage() throws Exception{
+    public void jsonMessage() throws Exception {
+        Person person = new Person();
+        person.setId(2018l);
+        person.setName("rldnddl");
+
+        String jsonString = objectMapper.writeValueAsString(person);
+
+        this.mockMvc.perform(get("/jsonMessage")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(jsonString))
+                .andDo(print())
+                .andExpect(status().isOk())
+                ;
+    }
+
+    @Test
+    public void stringMessage() throws Exception {
         this.mockMvc.perform(get("/hello")
                 .content("hello"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
     }
+
+
 }
