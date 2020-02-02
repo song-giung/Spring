84d65db3decdccd380e4fb74c11bd987d0460fd1
giungSong <rldnddl87@gmail.com>
2020-02-03 01:55:58 +0900

    no message

---
+++ spring-mvc/src/main/java/com/rldnddl87/springmvc/HelloServlet.java
@@ -0,0 +1,6 @@
+package com.rldnddl87.springmvc;
+
+import javax.servlet.http.HttpServlet;
+
+public class HelloServlet extends HttpServlet {
+}
---
+++ spring-mvc/src/main/java/com/rldnddl87/springmvc/WebConfig.java
@@ -0,0 +1,10 @@
+package com.rldnddl87.springmvc;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
