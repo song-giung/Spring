edaa50606c6d670afdd409c5827ba2b41b684acb
giungSong <rldnddl87@gmail.com>
2020-03-09 19:38:52 +0900

    DispatcherServlet - 계층형 구현

--- spring-mvc/basicservlet/src/main/java/me/rldnd/WebConfig.java
+++ spring-mvc/basicservlet/src/main/java/me/rldnd/WebConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
-
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-public class AppConfig {
-
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+public class WebConfig {
 }
