6e4fcdbf7bd5d7276638528da58824ce2bfdcce8
giungSong <rldnddl87@gmail.com>
2020-03-11 17:18:41 +0900

    Spring Boot환경에서 MVC설정 방법 1. application.properties를 사용 2. @Configuration + Implements WebMvcConfigurer

---
+++ spring-mvc/mvc1/src/main/java/com/demo/springmvc/WebConfig.java
@@ -0,0 +1,12 @@
+package com.demo.springmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc 활성화 시 스프링 부트의 Mvc 자동 설정을 사용하지 않는다.
+public class WebConfig implements WebMvcConfigurer {'
+
+    
+}
