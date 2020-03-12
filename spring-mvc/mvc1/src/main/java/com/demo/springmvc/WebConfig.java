993ad773ad3cbecc9a88fe8066f886be9abaddca
giungSong <rldnddl87@gmail.com>
2020-03-12 19:07:36 +0900

    오타 수정

--- spring-mvc/mvc1/src/main/java/com/demo/springmvc/WebConfig.java
+++ spring-mvc/mvc1/src/main/java/com/demo/springmvc/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc 활성화 시 스프링 부트의 Mvc 자동 설정을 사용하지 않는다.
-public class WebConfig implements WebMvcConfigurer {'
+public class WebConfig implements WebMvcConfigurer {
 
-    
+
 }
