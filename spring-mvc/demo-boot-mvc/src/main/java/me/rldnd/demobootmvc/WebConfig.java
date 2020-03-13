560b2498bc9dbb40a518ea4c05701ad5434a9fd4
giungSong <rldnddl87@gmail.com>
2020-03-13 16:21:25 +0900

    리소스 핸들러 설정

--- spring-mvc/demo-boot-mvc/src/main/java/me/rldnd/demobootmvc/WebConfig.java
+++ spring-mvc/demo-boot-mvc/src/main/java/me/rldnd/demobootmvc/WebConfig.java
@@ -2,8 +2,12 @@ package me.rldnd.demobootmvc;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GreetingInterceptor()).order(0); // 음수일수록 우선순위가 높다.
         registry.addInterceptor(new AnotherInterceptor()).addPathPatterns("/hi").order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/", "file:/Users") //클래스 패스와 파일시스템경로등 여러개를 설정할 수 있다.
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
 }
--- spring-mvc/demo-boot-mvc/src/test/java/me/rldnd/demobootmvc/SampleControllerTest.java
+++ spring-mvc/demo-boot-mvc/src/test/java/me/rldnd/demobootmvc/SampleControllerTest.java
@@ -1,15 +1,20 @@
 package me.rldnd.demobootmvc;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.regex.Matcher;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,5 +38,22 @@ class SampleControllerTest {
                 .andExpect(content().string("hello rldnd"));
     }
 
+    //Spring Boot의 정적 리소스 핸들러 사용
+    @Test
+    void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
 
+    //Custom 정적 리소스 핸들러 테스트
+    @Test
+    void mobileStatic() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+    }
 }
