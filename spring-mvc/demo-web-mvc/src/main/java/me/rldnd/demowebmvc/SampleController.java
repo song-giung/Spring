bb6a41557d3c55ce44210c874d7a612346816db3
giungSong <rldnddl87@gmail.com>
2020-03-15 15:27:39 +0900

    요청 맵핑하기 - head와 options 요청 : 스프링에서 요청을 자동으로 처리한다.

--- spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
+++ spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
@@ -3,23 +3,22 @@ package me.rldnd.demowebmvc;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping(value = "hello", headers = HttpHeaders.FROM + "=" + "111")
+    @GetMapping(value = "hello")
     @ResponseBody
     public String hello() {
         return "hello";
     }
 
-    //파라미터도 가능하다
-    //@RequestMapping(value = "hello", params = "name")
-
-    //결국 consumes와 produces 모두 headers로 맵핑할 수 있는 것이다.
+    @PostMapping(value = "hello")
+    @ResponseBody
+    public String helloPost() {
+        return "hello";
+    }
 }
--- spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
+++ spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,12 +23,23 @@ class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void hello() throws Exception{
-        this.mockMvc.perform(get("/hello")
-                        .header(HttpHeaders.FROM, "111"))
+    void headTest() throws Exception{
+        this.mockMvc.perform(head("/hello"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+
+        //head로 요청을 받을경우 본문이 없는 헤더정보만 리턴해야 한다.
+    }
+
+    @Test
+    void optionTest() throws Exception {
+        this.mockMvc.perform(options("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        //options로 요청을 받으면 헤더에 응답가능한 httpmethod목록을 제공한다.
+        // Headers = [Allow:"GET,HEAD,POST,OPTIONS"]
     }
 
 
