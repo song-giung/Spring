00c4903804fe26746c5cbc90755fd5798081b38f
giungSong <rldnddl87@gmail.com>
2020-03-15 14:45:23 +0900

    요청 맵핑하기 - 미디어 타입

--- spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
+++ spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
@@ -1,25 +1,25 @@
 package me.rldnd.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @Controller
-@RequestMapping(method = RequestMethod.GET) //클래스범위에서도 지정 가능 하다.
 public class SampleController {
 
-    @RequestMapping(value = {"/hello", "/hi"}, method = {RequestMethod.GET, RequestMethod.POST}) //http method에 상관없이 실행된다(get,put,delete..)
+    @RequestMapping(
+            value = "hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+                )
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
-    /*
-     * 요청 식별자
-     * ?(한 글자) : "/hello/???" -> "/hello/123"
-     * *(여러 글자) : "/hello/*" -> "/hello/rldnd"
-     * **(여러 패스) : "/hello/**" -> "/hello/rldnd/spring/boot"
-     * */
+
 }
--- spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
+++ spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,11 +23,16 @@ class SampleControllerTest {
 
     @Test
     void hello() throws Exception{
-        this.mockMvc.perform(get("/hello")) //@RequestMapping이기 때문에 HttpMethod에 상관없다.
+        this.mockMvc.perform(get("/hello")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    //.accept(MediaType.APPLICATION_JSON)
+                    )
                 .andDo(print())
                 .andExpect(status().isOk()) //status().isMethodNotAllowed()
                 .andExpect(content().string("hello"))
                 .andExpect(handler().handlerType(SampleController.class))
                 .andExpect(handler().methodName("hello"));
     }
+
+
 }
