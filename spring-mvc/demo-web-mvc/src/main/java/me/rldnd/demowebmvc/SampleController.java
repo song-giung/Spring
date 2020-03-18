b8083f080584a802c326c8d21138f62c111637b5
giungSong <rldnddl87@gmail.com>
2020-03-18 19:58:21 +0900

    핸들러 메소드 - @SessionAttributes - SessionStatus - @ModelAttribute는 Session에 있는 데이터도 바인딩 한다.

--- spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
+++ spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/SampleController.java
@@ -5,24 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("event") //배열 형식{} 으로 여러 값을 설정할 수 있고 Model의Attribute에 설정한 이름의 값이 담긴다면 자동으로 세션에도 담긴다.
 public class SampleController {
 
     @GetMapping("/events/form")
-    public String eventsForm(Model model) {
+    public String eventsForm(Model model, HttpSession httpSession) {
         Event newEvent = new Event();
         newEvent.setLimit(50);
         model.addAttribute("event", newEvent);
+        httpSession.setAttribute("event1", newEvent);
         return "/events/form";
     }
 
     @PostMapping("/events")
-    public String createEvents(@Valid @ModelAttribute Event event, BindingResult bindingResult) {
+    public String createEvents(@Valid @ModelAttribute Event event, BindingResult bindingResult, SessionStatus sessionStatus) {
         if (bindingResult.hasErrors()) {
            return "/events/form";
         }
@@ -30,6 +34,7 @@ public class SampleController {
         List<Event> eventList = new ArrayList<>();
         eventList.add(event);
         //db save
+        sessionStatus.setComplete();
         return "redirect:/events/lists";
     }
 
--- spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
+++ spring-mvc/demo-web-mvc/src/test/java/me/rldnd/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,10 +29,15 @@ class SampleControllerTest {
 
     @Test
     void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("/events/form"))
-                .andExpect(model().attributeExists("event"));
+                .andExpect(model().attributeExists("event"))
+                .andExpect(request().sessionAttribute("event", notNullValue()))
+                .andReturn().getRequest();
+
+        Object obj = request.getSession().getAttribute("event");
+        System.out.println(obj);
     }
 
 
