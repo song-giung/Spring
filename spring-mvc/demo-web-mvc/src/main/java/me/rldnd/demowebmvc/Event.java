3a9bbfe550f6227e7ab4919451312ec6b68eea90
giungSong <rldnddl87@gmail.com>
2020-03-24 16:14:25 +0900

    @InitBinder - 바인딩 설정 - 포매터 설정 - Validator 설정

--- spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/Event.java
+++ spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/Event.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Event {
-
-
 
     private Integer id;
 
@@ -23,4 +23,7 @@ public class Event {
 
     @Min(0)
     private Integer limit;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    public LocalDate startDate;
 }
--- spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/EventController.java
+++ spring-mvc/demo-web-mvc/src/main/java/me/rldnd/demowebmvc/EventController.java
@@ -3,6 +3,7 @@ package me.rldnd.demowebmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -17,6 +18,11 @@ import java.util.List;
 @Controller
 @SessionAttributes("event")
 public class EventController {
+
+    @InitBinder
+    public void initEventBinder(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
+    }
 
     @ModelAttribute
     public void categories(Model model) {
