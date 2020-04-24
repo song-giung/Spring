5a8b6d6897be0fa3375835e621d01632ce01739a
giungSong <rldnddl87@gmail.com>
2020-04-24 15:00:34 +0900

    Http2사용하려면  - SSL적용  - 필요한 경우 서블릿컨테이너 별로 설정을 추가 해야함

--- spring-boot-webserver/src/main/java/me/giung/springbootwebserver/SpringBootWebserverApplication.java
+++ spring-boot-webserver/src/main/java/me/giung/springbootwebserver/SpringBootWebserverApplication.java
@@ -18,20 +18,6 @@ public class SpringBootWebserverApplication {
         return "Hello Spring";
     }
 
-    @Bean
-    public ServletWebServerFactory serverFactory(){
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(creteStandardConnector());
-        return tomcat;
-    }
-
-    //https적용시 http통신은 막히게 된다
-    //enable https + http
-    private Connector creteStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8090);
-        return connector;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebserverApplication.class, args);
