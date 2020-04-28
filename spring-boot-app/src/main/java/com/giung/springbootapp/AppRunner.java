e1fd8a01a4cab2713dd63cd35b16480ae797c82b
giungSong <rldnddl87@gmail.com>
2020-04-28 20:57:01 +0900

    기본적인 로깅

--- spring-boot-app/src/main/java/com/giung/springbootapp/AppRunner.java
+++ spring-boot-app/src/main/java/com/giung/springbootapp/AppRunner.java
@@ -1,5 +1,7 @@
 package com.giung.springbootapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,8 +16,11 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     giungProperties properties;
 
+    private Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        logger.info("-=-=-=-=-=-=-=-=-=-==-=-=");
         System.out.println(hello);
         System.out.println(properties.getName());
     }
