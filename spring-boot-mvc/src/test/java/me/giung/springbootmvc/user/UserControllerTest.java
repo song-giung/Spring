8ac604953aaabc309e7c0f2ac7cf535c8fb39183
giungSong <rldnddl87@gmail.com>
2020-05-07 23:06:10 +0900

    Spring MVC - HttpMessageConverter - json

---
+++ spring-boot-mvc/src/test/java/me/giung/springbootmvc/user/UserControllerTest.java
@@ -0,0 +1,52 @@
+package me.giung.springbootmvc.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string("hello"));
+    }
+
+
+    @Test
+    public void createUser_JSON() throws Exception {
+
+
+        String userJson = "{\"username\":\"giung\", \"password\":\"123\"}";
+
+
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(userJson))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.username", is(equalTo("giung"))))
+                    .andExpect(jsonPath("$.password", is(equalTo("123"))));
+
+
+    }
+
+
+}
