490714aacd1cf55be5e2f56f4b9af99e37427389
giungSong <rldnddl87@gmail.com>
2020-05-07 15:21:01 +0900

    MockBean 어노테이션의 사용 - 실제 Service에 등록된 빈이 아닌 테스트시에는 MockBean이 작동한다.(커스텀하게 수정이 가능한..)

--- spring-boot-test/src/test/java/me/giung/springboottest/sample/SampleControllerTest.java
+++ spring-boot-test/src/test/java/me/giung/springboottest/sample/SampleControllerTest.java
@@ -5,28 +5,35 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     void hello() throws Exception{
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello giung"))
-                .andDo(print());
+        when(mockSampleService.getName()).thenReturn("whiteship");
+
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello whiteship");
     }
 }
