9ae39f0eea0d7df07dcaea96fec6fdd3d72e4c8b
giungSong <rldnddl87@gmail.com>
2020-05-07 15:14:54 +0900

    기본적인 WebMvc 테스트 방법 - 내가 작성한 컨트롤러를 테스트할때 - MockMvc를 사용하자.

---
+++ spring-boot-test/src/main/java/me/giung/springboottest/sample/SampleService.java
@@ -0,0 +1,10 @@
+package me.giung.springboottest.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    public String getName() {
+        return "giung";
+    }
+}
---
+++ spring-boot-test/src/test/java/me/giung/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.giung.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
