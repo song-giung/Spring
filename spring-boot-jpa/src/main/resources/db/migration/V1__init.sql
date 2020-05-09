02e167a1a3ff696da17c9cb3dd99eee1c3b85b04
giung <rldnddl87@gmail.com>
2020-05-09 15:15:21 +0900

    DB Migration & 형상관리 툴 - flyway의 간략한 사용법

---
+++ spring-boot-jpa/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,9 @@
+drop table if exists account cascade;
+drop sequence if exists hibernate_sequence;
+create sequence hibernate_sequence start 1 increment 1;
+create table account (
+  id int8 not null,
+  password varchar(255),
+  user_name varchar(255),
+  primary key (id)
+);
